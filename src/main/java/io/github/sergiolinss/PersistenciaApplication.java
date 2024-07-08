33004322c55f8caf49e4de8d83e5dd513173bb52
sergiolinss <sergiovictor2000@outlook.com>
2024-07-08 15:04:33 -0300

    criação da aplicação e config de acesso ao banco de dados

---
+++ src/main/java/io/github/sergiolinss/PersistenciaApplication.java
@@ -0,0 +1,13 @@
+package io.github.sergiolinss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class PersistenciaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PersistenciaApplication.class, args);
+    }
+}
