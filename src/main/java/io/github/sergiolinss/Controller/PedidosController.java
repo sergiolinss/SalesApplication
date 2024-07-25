8b0b3a9e4fb0c5e76510a23aaffba83e301d5250
Sergio <sergiovictor2000@outlook.com>
2024-07-24 22:48:22 -0300

    criação do SavePedido

--- src/main/java/io/github/sergiolinss/Controller/PedidosController.java
+++ src/main/java/io/github/sergiolinss/Controller/PedidosController.java
@@ -1,8 +1,10 @@
 package io.github.sergiolinss.Controller;
 
+import io.github.sergiolinss.DTOs.PedidoDTO;
+import io.github.sergiolinss.Entity.Pedido;
 import io.github.sergiolinss.Service.PedidosService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/pedidos")
@@ -12,5 +14,13 @@ public class PedidosController {
 
     public PedidosController(PedidosService service) {
         this.service = service;
+    }
+
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Integer saveProduto (PedidoDTO pedidoDTO){
+        Pedido pedido = service.salvar(pedidoDTO);
+        return pedido.getId();
     }
 }
--- src/main/java/io/github/sergiolinss/Service/PedidosService.java
+++ src/main/java/io/github/sergiolinss/Service/PedidosService.java
@@ -1,4 +1,8 @@
 package io.github.sergiolinss.Service;
 
+import io.github.sergiolinss.DTOs.PedidoDTO;
+import io.github.sergiolinss.Entity.Pedido;
+
 public interface PedidosService {
+    Pedido salvar(PedidoDTO pedidoDTO);
 }
